d65d5dc90c3a42a855851c29483a1b17b8463695
Minenhle Ncapai <minipai64@gmail.com>
2024-10-04 01:28:54 +0200

    Fix: Replaced taskElement.click() with addEventListener for better event handling

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function filterAndDisplayTasksByBoard(boardName) {
       taskElement.setAttribute('data-task-id', task.id);
 
       // Listen for a click event on each task and open a modal
-      taskElement.click() => { 
+      taskElement.addEventListener("click", () => { // FIX: Changed taskElement.click() to addEventListener 
         openEditTaskModal(task);
       });
 
